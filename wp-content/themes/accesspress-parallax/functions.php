145c9cf4f61e1f543bff10d881b9e2622ece7cff
foedus <andrew.smeall@gmail.com>
2015-02-16 15:23:07 +0000

    removed extra footers from footer registration

--- wp-content/themes/accesspress-parallax/functions.php
+++ wp-content/themes/accesspress-parallax/functions.php
@@ -99,36 +99,6 @@ function accesspress_parallax_widgets_init() {
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	) );
-
-	register_sidebar( array(
-		'name'          => __( 'Footer Two', 'accesspress_parallax' ),
-		'id'            => 'footer-2',
-		'description'   => '',
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</aside>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
-
-	register_sidebar( array(
-		'name'          => __( 'Footer Three', 'accesspress_parallax' ),
-		'id'            => 'footer-3',
-		'description'   => '',
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</aside>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
-
-	register_sidebar( array(
-		'name'          => __( 'Footer Four', 'accesspress_parallax' ),
-		'id'            => 'footer-4',
-		'description'   => '',
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</aside>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
 }
 add_action( 'widgets_init', 'accesspress_parallax_widgets_init' );
 
