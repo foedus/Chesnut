47a362472e22a1b97e8430b5d57a55ba0d62b54d
foedus <andrew.smeall@gmail.com>
2015-03-10 14:46:25 +0000

    formatting of library page

--- wp-content/themes/chestnut-theme/library-page.php
+++ wp-content/themes/chestnut-theme/library-page.php
@@ -11,26 +11,35 @@ get_header(); ?>
 <div class="mid-content">
 	<div id="page-primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			<div id="library-filters" class="content-area">
-				<h1>Filters</h1>
-			</div>
+			<aside id="library-filters-column">
+				<div id="library-filters">
+					<h2>Filters</h2>
+					<form id="filter-items">
+						<input type="checkbox" name="option" value="1">Option 1
+						<br>
+						<input type="checkbox" name="option" value="2">Option 2
+					</form>
+				</div>
+			</aside>
 
-			<?php
-				$args = array( 'posts_per_page' => 10, 'order'=> 'ASC', 'orderby' => 'title', 'category_name' => 'Library' );
-				$postslist = get_posts( $args );
-				foreach ( $postslist as $post ) :
-			  		setup_postdata( $post ); ?> 			
-					<div class="course-info">
-						<div id="course-image"><?php the_post_thumbnail(); ?></div>
-						<div id="course-text">
-							<h2 id="course-title"><?php the_title(); ?></h2>
-							</br>
-							<?php the_excerpt(); ?>
+			<div id="course-listing-column">
+				<?php
+					$args = array( 'posts_per_page' => 10, 'order'=> 'ASC', 'orderby' => 'title', 'category_name' => 'Library' );
+					$postslist = get_posts( $args );
+					foreach ( $postslist as $post ) :
+				  		setup_postdata( $post ); ?> 			
+						<div class="course-info">
+							<div id="course-image"><?php the_post_thumbnail(); ?></div>
+							<div id="course-text">
+								<h2 id="course-title"><?php the_title(); ?></h2>
+								</br>
+								<?php the_excerpt(); ?>
+							</div>
 						</div>
-					</div>
-			<?php endforeach; 
-			wp_reset_postdata(); 
-			?>
+				<?php endforeach; 
+				wp_reset_postdata(); 
+				?>
+			</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
