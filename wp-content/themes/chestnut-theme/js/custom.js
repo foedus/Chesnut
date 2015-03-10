ba597d5ecc23c76e3e725945b2eec6eb5c2561f9
foedus <andrew.smeall@gmail.com>
2015-03-10 18:15:07 +0000

    trimmed unused js

--- wp-content/themes/chestnut-theme/js/custom.js
+++ wp-content/themes/chestnut-theme/js/custom.js
@@ -31,69 +31,8 @@ jQuery(document).ready(function($){
     $(this).css('margin-top',-(cap_height/2));
     });
 
-    }).resize();;
-
     $('#main-slider .overlay').prependTo('#main-slider .slides');
 
-    $('.testimonial-slider').bxSlider({
-        auto:true,
-        speed: 1000,
-        pause: 8000,
-        pager:false,
-        nextText: '<i class="fa fa-angle-right"></i>',
-        prevText: '<i class="fa fa-angle-left"></i>'
-    });
-
-    $('.team-slider').bxSlider({
-        auto:false,
-        pager:false,
-        nextText: '<i class="fa fa-angle-right"></i>',
-        prevText: '<i class="fa fa-angle-left"></i>',
-        moveSlides : 1,
-        minSlides: 2,
-        maxSlides: 7,
-        slideWidth: 140,
-        slideMargin: 20,
-        infiniteLoop: false,
-        hideControlOnEnd: true
-    });
-
-    $('.team-content .team-list:first').show();
-    $('.team-tab .team-image:first').addClass('active');
-
-    $('.team-tab .team-image').on('click', function(){
-        $('.team-image').removeClass('active');
-        $('.team-content .team-list').hide();
-        $(this).addClass('active');
-        var teamid = $(this).attr('id');
-        $('.team-content .'+teamid).fadeIn();
-        return false;
-    });
-
-    $(window).bind('load',function(){
-        $('.googlemap-content').hide();  
-    });
-    
-    var open = false;
-    $('.googlemap-toggle').on('click', function(){
-        if(!open){
-        open = true;
-        initialize();
-        }
-        $('.googlemap-content').slideToggle();
-        $(this).toggleClass('active');
-    });
-
-    $('.social-icons a').each(function(){
-    var title = $(this).attr('data-title')
-    $(this).find('span').text(title);
-    });
-
-    $('.gallery-item a').each(function(){
-        $(this).addClass('fancybox-gallery').attr('data-lightbox-gallery','gallery');
-    });
-    
-    $(".fancybox-gallery").nivoLightbox();
 
     $('.menu-toggle').click(function(){
         $(this).next('ul').slideToggle();
