174b9978cf8c4d39cbdf27c1ff6610f292662d50
foedus <andrew.smeall@gmail.com>
2015-03-13 15:54:42 +0000

    Removed iframe to allow youtube to load itself

--- wp-content/themes/chestnut-theme/course-page.php
+++ wp-content/themes/chestnut-theme/course-page.php
@@ -19,8 +19,6 @@ get_header(); ?>
 				<div class="entry-content">
 					<div id="video-player" style="width:100%;float:left;">
 						<div id="player" style="width:800px;height:450px;background-color:lightgrey;border:1px solid grey;">
-							<iframe width="800" height="450" src="https://www.youtube.com/embed/videoseries?list='<?php echo $playlistCode ?>'" frameborder="0" allowfullscreen>
-							</iframe>
 						</div>
 					</div>
 					<div id="course-info" style="width:600px;border:1px solid grey;float:left;">
@@ -42,25 +40,70 @@ get_header(); ?>
 </div>
 
 <script type="text/javascript">
+
 "use strict";
 var postJSON;
 var category = "<?php echo $pagename ?>".slice(0,5);
-console.log(category);
+var player;
+
+console.time("playerLoad");
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+function onYouTubeIframeAPIReady() {
+	player = new YT.Player('player', {
+		height: '450',
+		width: '800',
+		videoId: '',
+		playerVars: {
+			showinfo: '0',
+			origin: 'chestnut-learning.com'
+		},
+		events: {
+			'onReady': onPlayerReady,
+			'onStateChange': onPlayerStateChange
+		}
+	});
+}
+
+function onPlayerReady(event) {
+	console.timeEnd("playerLoad");
+	player.playVideo();
+}
+
+var done = false;
+function onPlayerStateChange(event) {
+	if (event.data == YT.PlayerState.PLAYING && !done) {
+		setTimeout(stopVideo, 6000);
+		done = true;
+	}
+}
+
+function stopVideo() {
+	player.stopVideo();
+}
+
 (function() {
+	console.time("dataLoad");
 	var myRequest = new XMLHttpRequest();
 	myRequest.onreadystatechange = function() {
 		if (myRequest.readyState === 4 && myRequest.status === 200) {
+			console.timeEnd("dataLoad");
 			postJSON = JSON.parse(myRequest.responseText);
 			postJSON.sort(function(a,b) { 
 				return parseInt(a["terms"]["post_tag"][0]["name"]) - parseInt(b["terms"]["post_tag"][0]["name"]); 
-			})
-			console.dir(postJSON);
+			});
+			document.getElementById("course-title").innerHTML = postJSON[0]["CourseID"];
 			document.getElementById("clip-title").innerHTML = postJSON[0]["title"];
+			player.loadVideoById(postJSON[0]["embedCode"]).playVideo();
 		}
 	}
 	myRequest.open("GET", "http://www.chestnut-learning.com/wp-json/posts?filter[post_per_page]=99&filter[category_name]="+category, true);
 	myRequest.send();
 })();
+
 </script>
 
 <?php get_footer(); ?>
